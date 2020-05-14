d91ab1deb2115a251d0eb948666defa319555110
Adam Jahr <adamjahr@gmail.com>
2020-05-14 04:43:10 -0400

    Vue 2 code to change over

---
+++ components/ReviewList.js
@@ -0,0 +1,20 @@
+Vue.component('review-list', {
+  template: `
+    <div class="review-container">
+      <p v-if="!reviews.length">There are no reviews yet.</p>
+      <ul v-else>
+        <li v-for="(review, index) in reviews" :key="index">
+          {{ review.name }} gave this {{ review.rating }} stars
+          <br/>
+          "{{ review.text }}"
+        </li>
+      </ul>
+    </div>
+  `,
+  props: {
+    reviews: {
+      type: Array,
+      required: true,
+    },
+  },
+})
---
+++ main.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    premium: true,
+    cart: [],
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id)
+    },
+  },
+})
