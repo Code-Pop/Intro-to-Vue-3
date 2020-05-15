3a601d079dd8f58b5c5bb065e040e9f374a59bad
Adam Jahr <adamjahr@Adams-iMac.local>
2020-05-15 00:30:15 -0400

    changed heading in review-list

--- components/ReviewList.js
+++ components/ReviewList.js
@@ -3,14 +3,17 @@ app.component('review-list', {
     /*html*/
     `
     <div class="review-container">
-      <p v-if="!reviews.length">There are no reviews yet.</p>
-      <ul v-else>
-        <li v-for="(review, index) in reviews" :key="index">
-          {{ review.name }} gave this {{ review.rating }} stars
-          <br/>
-          "{{ review.text }}"
-        </li>
-      </ul>
+      <h3 v-if="!reviews.length">There are no reviews yet.</h3>
+      <div v-else>
+      <h3>Reviews:</h3>
+        <ul>
+          <li v-for="(review, index) in reviews" :key="index">
+            {{ review.name }} gave this {{ review.rating }} stars
+            <br/>
+            "{{ review.text }}"
+          </li>
+        </ul>
+      </div>
     </div>
   `,
   props: {
