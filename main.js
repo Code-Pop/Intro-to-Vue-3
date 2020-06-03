b92a6c065c54a147c91c3b1d28a42e973e624691
Adam Jahr <adamjahr@Adams-iMac.local>
2020-06-02 20:33:19 -0400

    L4 starting code

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg'
+            image: './assets/images/socks_blue.jpg'
         }
     }
 })
