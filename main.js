a903dd19cbd4e23ffe6ba1f9bd69c878a1dfbc45
Adam Jahr <adamjahr@Adams-iMac.local>
2020-06-19 17:23:40 -0400

    L5 starting code

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inStock: true
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester']
         }
     }
 })
