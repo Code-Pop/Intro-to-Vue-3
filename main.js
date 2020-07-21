a2cbfb577f15399c81ddafe6917e79e725671c59
Adam Jahr <adamjahr@Adams-iMac.local>
2020-07-21 00:17:10 -0400

    L6 solution

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = Vue.createApp({
             cart: 0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
@@ -16,6 +16,13 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
+        // solution
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        // solution
         updateImage(variantImage) {
             this.image = variantImage
         }
