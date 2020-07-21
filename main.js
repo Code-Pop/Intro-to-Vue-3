8a27ba9b93df7eba863a49e829cf6e5d2bd25c26
Adam Jahr <adamjahr@Adams-iMac.local>
2020-07-20 23:45:43 -0400

    L3 solution

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg'
+            image: './assets/images/socks_green.jpg',
+            // solution
+            url: 'https://www.vuemastery.com/'
+            // solution
         }
     }
 })
