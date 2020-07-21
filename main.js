9076f8fea85428c7c2b256af4839685521cdca25
Adam Jahr <adamjahr@Adams-iMac.local>
2020-07-21 00:34:10 -0400

    L8 solution

--- main.js
+++ main.js
@@ -9,7 +9,10 @@ const app = Vue.createApp({
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ]
+            ],
+            // solution
+            onSale: true
+            // solution
         }
     },
     methods: {
@@ -29,6 +32,10 @@ const app = Vue.createApp({
         },
         inStock() {
             return this.variants[this.selectedVariant].quantity
+        },
+        sale() {
+            if (this.onSale)
+            return this.brand + ' ' + this.product + ' is on sale.'
         }
     }
 })
