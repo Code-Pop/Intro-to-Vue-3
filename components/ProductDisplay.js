18c7760cd490876d98de0746ebabfa4fa74604b4
Jim Burer <jburer@gmail.com>
2021-01-04 11:29:56 -0500

    L11

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -40,6 +40,8 @@ app.component('product-display', {
         </button>
 
       </div>
+      <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+      <review-form @review-submitted="addReview"></review-form>
     </div>
   </div>`,
   data() {
@@ -51,7 +53,8 @@ app.component('product-display', {
         variants: [
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
           { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-        ]
+        ],
+        reviews: []
     }
   },
   methods: {
@@ -60,6 +63,9 @@ app.component('product-display', {
       },
       updateVariant(index) {
           this.selectedVariant = index
+      },
+      addReview(review) {
+        this.reviews.push(review)
       }
   },
   computed: {
