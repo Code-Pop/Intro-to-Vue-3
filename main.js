34faee78481dacc9027ab1302030f1b74bd079d0
pitamer <itamar@codicate.com>
2021-01-23 02:21:30 +0200

    Now using v-for, both for product details and for the products themselves

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 const app = Vue.createApp({
-    data() {
-        return {
-          product: {
-            name: "Green Socks",
-            description: "They've seen things.",
-            image: "./assets/images/socks_green.jpg",
-            inStock: 50,
-            isOnSale: true
-          },
-        };
-    }
-})
+  data() {
+    return {
+      products: [
+        {
+          name: "Green Socks",
+          description: "They've seen things.",
+          details: ["50% cotton", "30% wool", "20% polyester"],
+          image: "./assets/images/socks_green.jpg",
+          inStock: 50,
+          isOnSale: true,
+        },
+        {
+          name: "Blue Socks",
+          description: "They've been places.",
+          details: ["50% cotton", "30% wool", "20% polyester"],
+          image: "./assets/images/socks_blue.jpg",
+          inStock: 8,
+          isOnSale: false,
+        },
+      ],
+    };
+  },
+});
