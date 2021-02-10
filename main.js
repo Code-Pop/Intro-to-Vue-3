7582717a7ba0acf6d9e05a76d952d8e008c53da2
Qunli <qunli02@gmail.com>
2021-02-10 13:49:59 -0500

    added even listeners

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
-    }
-})
+  data() {
+    return {
+      cart: 0,
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+      ],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateIamge(image) {
+      this.image = image;
+    },
+  },
+});
