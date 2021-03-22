408af7d368462c128d8e040564f085d96b33daf3
ashley <ashleyhuntdesign@gmail.com>
2021-03-22 12:51:06 -0500

    Complete lesson 1

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+// Vue is reactive and auto updates when changes are made
+
+const app = Vue.createApp({
+    data() {
+        return{
+            product: 'Socks',
+            description: 'A product that covers your feet so that you can wear shoes over them and prevent stinkiness.'
+        }
+    }
+})
