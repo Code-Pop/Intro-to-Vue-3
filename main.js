179fb60b79d90d4f55988c4e825639b97d340ad6
PholawatIostream <6206021622071@fitm.kmutnb.ac.th>
2021-04-07 08:44:23 +0700

    update

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const app = Vue.createApp({
     data(){
         return{
             product:'Socks',
-            image:'./assets/images/socks_blue.jpg'
+            image:'./assets/images/socks_blue.jpg',
+            url:'',
+            inStock: true,
+            inventory:100,
+            onSale:true
         }
     }
 })
