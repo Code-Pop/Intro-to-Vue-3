884d0113e6f1042a2642888240cc5639635dea35
PholawatIostream <52020781+PholawatIostream@users.noreply.github.com>
2021-04-07 11:21:42 +0700

    Add files via upload

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -1,9 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
-            premium:true
+            cart: [],
+            premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id){
+            this.cart +=1
+        }
+    }
 })
