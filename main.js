926359513cfdb445dd53c12e44d28bd6ffc05dca
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-04-11 11:38:49 +0100

    ✅

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', availableSizes: [5,7,8,9,10] },
+                { id: 2235, color: 'blue', availableSizes: [5,6,8,10,11] },
+            ]
         }
     }
 })
