c1cdeafaa73b3aa4ee536c913213e6aae6f3a9bd
jules-peak <83359465+jules-peak@users.noreply.github.com>
2021-04-28 18:05:28 +0200

    L2 solution

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Banana',
+            description: 'This is a yammy fruit.'
+        }
+    }
+})
