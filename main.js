dd49de47792c3a6a37fd0cad286a8062f1b0e66a
gregdavies91 <greg@facetothelight.com>
2021-07-29 16:57:09 +0800

    update

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ const app = Vue.createApp({
 	data() {
 		return {
 			product: 'Socks',
-			description: 'Socks are a very useful thing. Please wear them whenever you feel like it!',
-		}
-			
-					
+
+			description: 'Socks are a very useful thing. Please wear them whenever you feel like it!'
+		}			
 		
 	}
 })
