1a8508e3aaa0faa6807a3f907c9152545da9d46f
gregdavies91 <greg@facetothelight.com>
2021-07-29 19:42:54 +0800

    Lesson 4 completed challenge - my answer

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            image: './assets/images/socks_blue.jpg',
+            inventory: 0,
+            onSale: true
         }
     }
 })
