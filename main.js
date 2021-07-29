75ff1f005df560f463866d3f0f18632dc1583748
gregdavies91 <greg@facetothelight.com>
2021-07-29 20:35:22 +0800

    my answer to coding challenge lesson 5

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+            	{ id: 2234, color: 'green' },
+ 				{ id: 2235, color: 'blue' },           
+            ],
+            sizes: [
+            { id: 2236, size: 'small' },
+            { id: 2237, size: 'medium' },
+            { id: 2238, size: 'large' },
+            ]
         }
     }
 })
