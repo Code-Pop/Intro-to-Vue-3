789e4371f6bf392cca51e539d534172aa394b715
gregdavies91 <gregdaviesmusic@gmail.com>
2021-07-31 15:26:49 +0800

    l9 coding challenge complete

---
+++ components/ProductDetails.js
@@ -0,0 +1,18 @@
+app.component('product-details', {
+	props: {
+		details: {
+			type: 
+		}
+	},
+
+	data() {
+		return {
+			details: details
+		}
+	},
+
+	methods: {
+		getDetails() {
+			this.details = this.details
+		}
+	})
