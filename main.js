fcb5720fd836ee6e2b64b1a0664a5af1c03184f6
Kyle Martin <kyle.martin.0819@gmail.com>
2021-09-05 18:59:49 -0400

    Finished Lesson 3 - Intro to Vue 3

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            description: 'Tall green thick socks that are destined to keep your feet warm.',
+            url: 'http://127.0.0.1:5500/Intro-to-Vue-3/index.html'
         }
     }
 })
