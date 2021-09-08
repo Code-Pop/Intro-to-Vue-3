03ce910596b1da73a941b101a426a0d9476dca3b
DietrichWasHere <Dietrich.Versaw@eagles.oc.edu>
2021-09-08 17:55:04 -0500

    test
    test

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -41,7 +41,10 @@ app.component('product-display', {
 
       </div>
     </div>
+    <review-list :reviews="reviews"></review-list>
+    <review-form @review-submitted="addReview"></review-form>
   </div>`,
+  
   data() {
     return {
         product: 'Socks',
@@ -51,7 +54,8 @@ app.component('product-display', {
         variants: [
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
           { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-        ]
+        ],
+        reviews: []
     }
   },
   methods: {
@@ -60,6 +64,9 @@ app.component('product-display', {
       },
       updateVariant(index) {
           this.selectedVariant = index
+      },
+      addReview(review) {
+        this.reviews.push(review)
       }
   },
   computed: {
