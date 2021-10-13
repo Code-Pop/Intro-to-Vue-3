a0c5f06d50005f67aa0d30cdc7de61eedc4b1095
vaibhav <vaibhav@niltech.in>
2021-10-13 17:04:00 +0530

    L2 commit

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Ka ho',
+            description: 'This is discription of the product'
+        }
+    }
+})
