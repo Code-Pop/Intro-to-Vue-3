508481195c0f638af9ed2b64f24fa6932ec26063
Inbar Vernia <inbar.vernia@gmail.com>
2021-11-13 17:18:38 +0000

    Completed coding challenge: added onSale property to data object and used it to conditionally render a paragraph

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inventory: 6
+            inventory: 6,
+            onSale: false
         }
     }
 })
