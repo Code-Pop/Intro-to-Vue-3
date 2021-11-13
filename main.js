c89b9681fb0e557010b3e5c32a68b9447a4f102f
Inbar Vernia <inbar.vernia@gmail.com>
2021-11-13 17:06:54 +0000

    Replaced v-if with v-show so that element is rendered but not display (display set to none) instead of not rendered at all

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inStock: true
+            inStock: false
         }
     }
 })
