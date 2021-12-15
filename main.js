5a64b408d0ed75b7af6f9208efb437386e159a65
gzbotii <gzbotii@gmail.com>
2021-12-15 09:24:17 +0200

    A good starting point for a webshop project

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const app = Vue.createApp({
     data() {
         return {
             cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            product: 'Retran 5K',
+            brand: 'Volcam',
+            image: './assets/images/green_jacket.jpeg',
+            inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/green_jacket.jpeg' },
+              { id: 2235, color: 'black', image: './assets/images/black_jacket.jpeg' },
             ]
         }
     },
