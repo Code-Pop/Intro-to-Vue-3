9b72ad9cdbdb3682c069a24539cb61dcefe5c5f2
rb.tarek <rebaitarek54@gmail.com>
2021-12-17 17:31:18 +0100

    commit to l4-start

--- main.js
+++ main.js
@@ -7,7 +7,12 @@ const app = Vue.createApp({
 
             product:'Socks',
             image: './assets/images/socks_blue.jpg',
-            inventory: 0
+            inventory: 0,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+              ]
 
         }
      }
