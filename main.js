7df8ff20b4b57907e85703143eab940095a6bfd9
rb.tarek <rebaitarek54@gmail.com>
2021-12-17 17:58:58 +0100

    commit for l5-start

--- main.js
+++ main.js
@@ -6,16 +6,29 @@ const app = Vue.createApp({
         return{
 
             product:'Socks',
-            image: './assets/images/socks_blue.jpg',
             inventory: 0,
+            image:'./assets/images/socks_blue.jpg',
+            cart:0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' }
+                { id: 2234, color: 'green',image:'./assets/images/socks_blue.jpg'},
+                { id: 2235, color: 'blue',image:'./assets/images/socks_green.jpg'}
               ]
-
         }
+     },
+
+     methods:{
+        addToCart(){
+            this.cart +=1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        }
+
+
      }
+
+     
 
 
 
