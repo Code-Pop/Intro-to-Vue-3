444872ef86925b09cd8d8809ed30e32c23c79628
Musab Jolani <34215119+musabjolani@users.noreply.github.com>
2022-01-13 18:01:26 +0200

    end-Basic

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
-    }
-})
+  data() {
+    return {
+      counter: 0,
+      product: "Socks",
+      img: "./assets/images/socks_blue.jpg",
+      selectedColor: "blue",
+
+      products: [
+        {
+          name: "prod 1",
+          color: "green",
+          size: "3x",
+        },
+        {
+          name: "prod 2",
+          color: "blue",
+          size: "Mx",
+        },
+      ],
+    };
+  },
+  methods: {
+    changeImg(color) {
+      this.selectedColor = color;
+    },
+    increasingCounter(num) {
+      this.counter = this.counter + num;
+    },
+    addingProduct(prod) {
+      console.log(prod);
+      //let product = null;
+      let product = [];
+      product.name = prod.name;
+      product.color = prod.color;
+      product.size = prod.size;
+      this.products.push(product);
+    },
+  },
+});
