46337e168fc19be697251b088da51fa84a26612b
Brady Williams <williams.cole29@yahoo.com>
2022-02-24 15:35:11 -0600

    Finished Lesson 2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return{
+            product: 'Socks',
+            description : 'Socks keep your feet warm'
+        }
+    }
+})
