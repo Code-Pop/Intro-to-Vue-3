d3f24a459d5e79f246143a596b265aecf5415b5c
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-03-16 02:34:00 -0400

    added comment

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+//What I passed inside the parenthesis are the options object. If I'm not going to pass anything to it I have to put in empty {}
 const app = Vue.createApp({
     data () {
         return {
