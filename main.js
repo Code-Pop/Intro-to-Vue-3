1e816dab11cbd936c4f69ba6498d1d20fb99f8f8
seanursua <seandoge@gmail.com>
2022-03-23 01:46:05 +0800

    L4

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            // solution
-            url: 'https://www.vuemastery.com/'
-            // solution
+            image: './assets/images/socks_blue.jpg',
+            inventory: 100,
+            onSale: true
         }
     }
 })
