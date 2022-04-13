e67071d11d50898e6b5a9f2484ff4dea905f4e69
Kevin Xiao <kevinxiao@Kevins-MacBook-Pro.local>
2022-04-12 19:55:45 -0700

    finish l2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'some warm feet covers',
+        }
+    }
+})
