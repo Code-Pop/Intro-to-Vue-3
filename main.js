b0af2bd0f489cc440dd7c2473be7437e0e5b3fc4
Jardeson Santos <JarDeVSon@github.com>
2022-04-19 17:05:51 -0300

    Attribute Binding

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+
+
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg'
+        
+        }
+    }
+})
