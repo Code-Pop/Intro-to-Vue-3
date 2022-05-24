2603bad02ead3097ad4693826f81d2ab6974d6aa
Emicaama <69168104+Emicaama@users.noreply.github.com>
2022-05-24 17:52:25 -0300

    L4 end

--- main.js
+++ main.js
@@ -2,7 +2,20 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            image: './assets/images/socks_blue.jpg',
+            inventory: 50,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% Polyester'],
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'},
+            ],
+            sizes: [
+                { id: 22340, size: 'S'},
+                { id: 22341, size: 'M'},
+                { id: 22342, size: 'L'},
+            ]
+
         }
     }
 })
