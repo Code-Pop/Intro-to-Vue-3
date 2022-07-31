eec569c1c9dd7e30c17a46c4b0720d9f0fdb90e8
James Schulze <James.Schulze@PlexsoftApps.com>
2022-07-30 22:25:26 -0500

    L3 done

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const app = Vue.createApp({
         return {
             product: {
                 title: 'Socks',
-                description: 'Nice socks!'
+                description: 'Nice socks!',
+                image: './assets/images/socks_green.jpg',
+                url: 'https://github.com/JamesASchulze/Intro-to-Vue-3/blob/master/assets/images/socks_green.jpg'
             }
         }
     }
