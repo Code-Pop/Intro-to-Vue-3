124ebfd992085c9d188f5cf3c159bd9cf765680b
James Schulze <James.Schulze@PlexsoftApps.com>
2022-07-30 21:46:15 -0500

    L2 finished

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data: function() {
         return {
-            product: 'Socks'
+            product: {
+                title: 'Socks',
+                description: 'Nice socks!'
+            }
         }
     }
 })
