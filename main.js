a6b6db4d2d04fdb5bf89697132e1f89e761521be
James Schulze <James.Schulze@PlexsoftApps.com>
2022-08-05 22:38:12 -0400

    Lesson 4: Conditional Rendering

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ const app = Vue.createApp({
                 title: 'Socks',
                 description: 'Nice socks!',
                 image: './assets/images/socks_green.jpg',
-                url: 'https://github.com/JamesASchulze/Intro-to-Vue-3/blob/master/assets/images/socks_green.jpg'
+                url: 'https://github.com/JamesASchulze/Intro-to-Vue-3/blob/master/assets/images/socks_green.jpg',
+                inventory: 9,
+                onSale: true
             }
         }
     }
