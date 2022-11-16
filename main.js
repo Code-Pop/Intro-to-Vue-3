b6f805d06124b61c3295360d5720987c690ae02f
Rachel Kelly <racheldk126@gmail.com>
2022-11-16 15:02:33 -0800

    end of lesson 3

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function(){
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg', 
+            url: 'http://placekitten.com/200/300',
+        }
+    }
+})
