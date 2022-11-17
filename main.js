3e29d22b2a39131abb16a108818022e0690cb2ee
Kei <marios_bestfriend@htmail.com>
2022-11-17 15:57:37 -0500

    Add image and invetory

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-const product = 'Socks'
+// const product = 'Socks'
+
+const app = Vue.createApp({
+    data() {                //ES6 Shorthand for data: () => {
+        return {
+            product: 'Socks',
+            description: 'Green and Soft',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://static.nrdbassets.com/v1/large/30001.jpg',
+            inventory: 100,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester']
+        }
+    }
+})
