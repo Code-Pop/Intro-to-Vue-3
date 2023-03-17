d70904723564f483e3c3d6249891e7460d2c75e1
Saray Obama <plankjim3@gmail.com>
2023-03-17 22:05:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
-<<<<<<< Updated upstream
-            product: 'Socks'
-=======
+
             product: 'FAT people',
             // Solution
             description: 'THE GREATEST EVIL OF THE WORLD'
             // Solution
->>>>>>> Stashed changes
+
         }
     }
 })
