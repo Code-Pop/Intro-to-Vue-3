591ef11b22dbd5ae7952b789a7a49f06a0bae94a
sanmerkt <sandro.merkt@hapa.ch>
2023-05-24 16:43:59 +0200

    Revert "Creating the Vue App"
    This reverts commit fff13a94149c9ed81b923b2351f4015da1a33d06.

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            description: 'That product got rewarded a 5 star review!'
-        }
-    }
-})
+const product = 'Socks'
