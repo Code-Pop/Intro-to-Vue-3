6d6b57d4527aea08953d1765fa437e81bcbe1aa3
gabcbiz <gabc.biz@gmail.com>
2023-06-14 21:02:58 +0800

    Lesson 8

--- main.js
+++ main.js
@@ -2,14 +2,13 @@ const app = Vue.createApp({
     data() {
         return {
             cart:0,
-            product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            product: 'Socks',
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: true },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false },
             ]
         }
     },
@@ -17,8 +16,25 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+
+        ifSale() {
+            return this.brand +  ' ' + this.product + ' is on sale'
         }
     }
 })
