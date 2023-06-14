548b405f4beb1144b775e0b5b49edf9c1f4b3eb3
gabcbiz <gabc.biz@gmail.com>
2023-06-14 20:10:11 +0800

    Lesson 6

--- main.js
+++ main.js
@@ -11,5 +11,18 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
+        },
+        removeFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1;
+            }
+        }
     }
 })
