1e11421b106263d69541d36ea716c24e6390817e
gabcbiz <gabc.biz@gmail.com>
2023-06-14 17:35:49 +0800

    Lesson 5 Code Challenge

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'bloo' },
+                { id: 2235, color: 'fenk' }
+            ],
+            sizes: ['Small', 'Medium', 'Large'],
+            genders: [
+                { id: 5246, value: 'Male' },
+                { id: 5247, value: 'Female' },
+                { id: 5248, value: 'Non-Binary' }
+            ]
         }
     }
 })
