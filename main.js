3de8e58b1224fa2785b00750eb65b38eecd36f74
gabcbiz <gabc.biz@gmail.com>
2023-06-14 22:09:06 +0800

    Lesson 10
    Removing item from cart

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        removeFromCart() {
+            this.cart.pop()
+        }
+    }
 })
