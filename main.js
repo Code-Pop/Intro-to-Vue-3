95b776ca0abcc7ff6c08e85acd335bf9b859cfb7
agathelurquin <agathe.lurquin@essca.eu>
2023-08-19 18:05:04 +0200

    conditional rendering

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      productDescription:
+        "Comfortable socks built with resistant and breathable material ",
+      image: "./assets/images/socks_green.jpg",
+      url: "https://www.amazon.com/socks/s?k=socks",
+      inStock: true,
+      inventory: 0,
+      onSale: true,
+    };
+  },
+});
