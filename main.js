773cdc893f30b7299984a6bcdf1ae5630cd41661
agathelurquin <agathe.lurquin@essca.eu>
2023-08-20 17:48:42 +0200

    computed properties

--- main.js
+++ main.js
@@ -4,12 +4,23 @@ const app = Vue.createApp({
       cart: 0,
       product: "Socks",
       brand: "Vue Mastery",
-      image: "./assets/images/socks_blue.jpg",
-      inStock: false,
+      selectedVariant: 0,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 0,
+          onSale: false,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 12,
+          onSale: true,
+        },
       ],
     };
   },
@@ -17,8 +28,26 @@ const app = Vue.createApp({
     addToCart() {
       this.cart += 1;
     },
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
+      console.log(index);
+    },
+  },
+
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+
+    onSale() {
+      return this.variants[this.selectedVariant].onSale;
     },
   },
 });
