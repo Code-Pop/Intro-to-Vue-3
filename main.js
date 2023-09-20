291fe1339259406718be4198ee68d4a3ccd16114
Stephanie <stephaniekou@yahoo.com>
2023-09-20 20:46:01 +0300

    l4 modifications

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-const app = Vue.createApp({
-    data() {
+const app= Vue.createApp({
+    data(){
         return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            product:'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
+            inStock:true
         }
     }
 })
