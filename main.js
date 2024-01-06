dff9a8cf3a6f544179396605021d847f4291b37b
nnnnadia <tristaonadia@gmail.com>
2024-01-06 18:16:10 -0300

    Lesson 5: List Rendering

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const app = Vue.createApp({
     data() {
-        return {
+        return ({
             product: 'Socks',
             description: 'Warms your feet.',
             image: './assets/images/socks_blue.jpg',
@@ -8,6 +8,11 @@ const app = Vue.createApp({
             inventory: 100,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
-        }
+            variants: [
+              { id: 2234, color: "green" },
+              { id: 2235, color: "blue" },
+            ],
+            sizes: [35, 36, 37, 38, 39, 40],
+        });
     }
 });
