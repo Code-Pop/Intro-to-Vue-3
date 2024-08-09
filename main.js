16663ede629ae569dac64e8b77b67ba3381e3204
Patryk Lubinski <p.lubinski@yahoo.com>
2024-08-09 10:30:59 +0200

    lesson 2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'A warm fuzzy pair of socks'
+        }
+    }
+})
