10312701de39192467e0dd7f9edce9526b98f698
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2025-01-30 21:33:03 +0200

    Create Vue app with CDN link This is great for prototyping, not for Prod

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+// const product = 'Socks'
+
+const app = Vue.createApp({
+    data() {
+        return{
+            product: 'Socks',
+            description: 'Wonderful woolen socks offered in two colours for you to choose from'
+        }
+    }
+})
