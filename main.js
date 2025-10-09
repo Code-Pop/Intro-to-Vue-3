5d7b3b4778c7354427b8bff9087aebfc75fc8c0f
Naji <naji.barri@legit.eu>
2025-10-09 13:18:31 +0200

    new

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             Image: './assets/images/socks_green.jpg',
-        }
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }]
+              
+              }
     }
 })
