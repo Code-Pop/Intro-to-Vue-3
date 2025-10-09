95994ce1596f6d5f6f2b9ee743324bea419e5adf
naji <naji.barri@legit.eu>
2025-10-09 12:27:01 +0200

    add image

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            Image: './assets/images/socks_green.jpg',
         }
     }
 })
